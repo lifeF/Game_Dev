7a08dae1ce6844e1e730b24226f58a0e1a32eb30
Kalana <edata.incore@gmail.com>
2019-01-08 22:17:53 +0530

    file structure

---
+++ Assets/Tests_PlayMode/TestCamaraMovement.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+
+public class TestCamaraMovement : MonoBehaviour  {
+    // A UnityTest behaves like a coroutine in PlayMode
+    // and allows you to yield null to skip a frame in EditMode
+    [UnityTest]
+    public IEnumerator TestCamaraMovementWithEnumeratorPasses() {
+        Setup();
+        yield return new WaitForSeconds(5);   
+    }
+
+    void  Setup(){
+        Instantiate(Resources.Load<GameObject>("Plane"));
+        Instantiate(Resources.Load<GameObject>("Main Camera"));
+        Instantiate(Resources.Load<GameObject>("Directional Light"));
+    }
+}
